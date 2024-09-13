5f259e94041b8a10e332f77027b257b69787bce8
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-09-14 00:57:05 +0300

    Initial commit

---
+++ CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSharp._4kyu;
+
+namespace CSharp;
+
+public class Program
+{
+    /// <summary>
+    /// Call the required method here.
+    /// In each solved Kata, the namespace and the name of the class are changed here. Be careful if you decide to copy the solution.
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine(SumStringsAsNumbers.sumStrings("1", "1"));
+    }
+}
