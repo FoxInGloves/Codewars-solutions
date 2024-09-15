c2f1a6f8e0cd03c76b2d1cbb8ff23a49ad9bfea0
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-09-15 22:43:50 +0300

    solved "Weight for weight"

---
+++ CSharp/5kyu/WeightForWeight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp._5kyu;
+
+public class WeightForWeight
+{
+    public static string orderWeight(string strng) {
+        var nums = strng.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+        var weightNums = new int[nums.Length];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            weightNums[i] = nums[i].ToString().Sum(c => c - '0');
+        }
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (weightNums[i] > weightNums[i + 1] || 
+                weightNums[i] == weightNums[i + 1] & string.CompareOrdinal(nums[i].ToString(), nums[i + 1].ToString()) > 0)
+            {
+                (weightNums[i], weightNums[i + 1]) = (weightNums[i + 1], weightNums[i]);
+                (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+                i = -1;
+            }
+        }
+
+        return string.Join(" ", nums);
+    }
+}
