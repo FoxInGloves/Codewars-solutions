b1b1dab990da754abb46a802f4f032300280c984
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-09-15 10:27:01 +0300

    solved "Count the smiley faces"

---
+++ CSharp/6kyu/CountTheSmileyFaces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CSharp._6kyu;
+
+public class CountTheSmileyFaces
+{
+    public static int CountSmileys(string[] smileys) 
+    {
+        return smileys.Count(x => Regex.IsMatch(x, @"^(:|;)\S?(\)|D)$"));
+    }
+    
+}
