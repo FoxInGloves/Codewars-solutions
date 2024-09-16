ccba61e10bc81cf91c5405ebcd5d4d913de37604
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-09-16 21:24:41 +0300

    solved "Sort the odd"

---
+++ CSharp/6kyu/SortTheOdd.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp._6kyu;
+
+public class SortTheOdd
+{
+    public static int[] SortArray(int[] array)
+    {
+        for (var first = 0; first < array.Length; first++)
+        {
+            for (var second = first; second < array.Length; second++)
+            {
+                if (array[first] % 2 == 1 && array[second] % 2 == 1 && array[first] > array[second])
+                {
+                    (array[first], array[second]) = (array[second], array[first]);
+                }
+            }
+        }
+
+        return array;
+    }
+}
