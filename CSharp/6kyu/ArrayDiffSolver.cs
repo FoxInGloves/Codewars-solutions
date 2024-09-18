60d940f1bc5d2b17d45d6853b0f879efada897f9
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-09-18 22:50:28 +0300

    solved "Array diff"

---
+++ CSharp/6kyu/ArrayDiffSolver.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp._6kyu;
+
+public class ArrayDiffSolver
+{
+    public static int[] ArrayDiff(int[] a, int[] b)
+    {
+        List<int> list = new List<int>();
+        list.AddRange(a);
+        foreach(int i in b)
+        {
+            foreach(int g in a)
+            {
+                if(i == g)
+                    list.Remove(i);
+            }
+        }
+        int[] ret = list.ToArray();
+        return ret;
+    }
+}
