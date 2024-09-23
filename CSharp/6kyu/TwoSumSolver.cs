bd75703d63c5838ec00515b5affe3814ab78c67f
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-09-23 22:04:30 +0300

    solved "Two sum"

---
+++ CSharp/6kyu/TwoSumSolver.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharp._6kyu;
+
+public class TwoSumSolver
+{
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        int[] result = new int[2];
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            for (var j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] + numbers[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
