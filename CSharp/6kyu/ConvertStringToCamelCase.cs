323506016f92d7554e3d29116b61a4a3e8158d57
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-10-12 20:33:14 +0300

    solved Convert string to camel case

---
+++ CSharp/6kyu/ConvertStringToCamelCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp._6kyu;
+
+public class ConvertStringToCamelCase
+{
+    public static string ToCamelCase(string str)
+    {
+        return str.Split(new [] {'-', '_'}).First() + string.Concat(str.Split(new [] {'-', '_'}).Skip(1)
+            .Select(x => char.ToUpper(x.ToCharArray()
+                .First()) + x.Remove(0,1)));
+    }
+}
