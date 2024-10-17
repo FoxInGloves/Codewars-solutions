5fee638caa16b923f4e94abc3d4a794383937092
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-10-17 21:36:41 +0300

    solved "FindTheOddInt"

---
+++ CSharp/6kyu/FindTheOddInt.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp._6kyu;
+
+public class FindTheOddInt
+{
+    public static int find_it(int[] seq) 
+    {
+        foreach (var i in seq)
+        {
+            if(seq.Count(x => x == i) % 2 != 0)
+                return i;
+        }
+        return -1;
+    }
+}
