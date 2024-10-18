b5ff629649c1092f61caabe1f6a85f563ced181b
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-10-18 17:11:42 +0300

    solved "BreakCamelCase"

---
+++ CSharp/6kyu/BreakCamelCaseSolver.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharp._6kyu;
+
+public class BreakCamelCaseSolver
+{
+    public static string BreakCamelCase(string str)
+    {
+        string result = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsUpper(str[i]))
+            {
+                result += " " + str.Substring(0, i);
+                str = str.Substring(i, str.Length - i);
+                i = 0;
+            }
+        }
+        return result.Trim() + " " + str;
+    }
+}
